1d60a4456df52b88d5cd781e641c434520cb34ee
ElenaPrisacari <lenaprisacari111@gmail.com>
2023-03-23 13:45:31 +0200

    Scheldule

--- src/components/Pages/ScheldulePage/ScheldulePage.js
+++ src/components/Pages/ScheldulePage/ScheldulePage.js
@@ -3,7 +3,171 @@ import "./ScheldulePage.css";
 
 class ScheldulePage extends React.Component {
     render() {
-        return <div>SchelduledPage</div>;
+        return <div>
+             <div class="Schedule">
+            <span class="s1">
+                Расписание звонков на 2020-2021 учебный год (I смена):
+            </span>
+            <table class="orartable">
+                <tr class="time">
+                    <td class="orar12">Урок</td>
+                    <td class="time1">Период</td>
+                    <td class="Recriatie">Перемена</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">I</td>
+                    <td class="time1">8:00 - 8:45</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">II</td>
+                    <td class="time1">8:55 - 9:40</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">III</td>
+                    <td class="time1">9:50 - 10:35</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">IV</td>
+                    <td class="time1">10:45 - 11:30</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">V</td>
+                    <td class="time1">11:40 - 12:25</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">VI</td>
+                    <td class="time1">12:35- 13:20</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+            </table>
+            <span class="s2">
+                Расписание звонков на 2020-2021 учебный год (II смена):</span
+            >
+            <table class="orartable1">
+                <tr class="time">
+                    <td class="orar12">Урок</td>
+                    <td class="time1">Период</td>
+                    <td class="Recriatie">Перемена</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">I</td>
+                    <td class="time1">13:40 - 14:25</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">II</td>
+                    <td class="time1">14:35 - 15:20</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">III</td>
+                    <td class="time1">15:30 - 16:15</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">IV</td>
+                    <td class="time1">16:25 - 17:10</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">V</td>
+                    <td class="time1">17:20 - 18: 05</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">VI</td>
+                    <td class="time1">18:15 - 19:00</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+            </table>
+            <span class="s3">
+                Расписание звонков для сокращенных уроков (I смена):</span
+            >
+            <table class="orartable2">
+                <tr class="time">
+                    <td class="orar12">Урок</td>
+                    <td class="time1">Период</td>
+                    <td class="Recriatie">Перемена</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">I</td>
+                    <td class="time1">8:00 - 8:35</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">II</td>
+                    <td class="time1">8:45 - 9:20</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">III</td>
+                    <td class="time1">9:30 - 10:05</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">IV</td>
+                    <td class="time1">10:15 - 10:50</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">V</td>
+                    <td class="time1">11:00 - 11:35</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">VI</td>
+                    <td class="time1">11:45 - 12:20</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+            </table>
+            <span class="s4">
+                Расписание звонков для сокращенных уроков (II смена):</span
+            >
+            <table class="orartable3">
+                <tr class="time">
+                    <td class="orar12">Урок</td>
+                    <td class="time1">Период</td>
+                    <td class="Recriatie">Перемена</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">I</td>
+                    <td class="time1">12:40 - 13:15</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">II</td>
+                    <td class="time1">13:25 - 14:00</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">III</td>
+                    <td class="time1">14:10 - 14:45</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">IV</td>
+                    <td class="time1">14:55 -15:30</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">V</td>
+                    <td class="time1">15:40 - 16:15</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+                <tr class="time">
+                    <td class="orar12">VI</td>
+                    <td class="time1">16:25 - 17:00</td>
+                    <td class="Recriatie">10 минут</td>
+                </tr>
+            </table>
+        </div>
+        <div class="lines"></div>
+        </div>;
     }
 }
 
