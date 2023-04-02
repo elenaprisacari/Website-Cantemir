cb7b8ebd90cd1bc427242172068ebe87633d86d8
ElenaPrisacari <lenaprisacari111@gmail.com>
2023-04-03 01:22:53 +0300

    teachers

--- src/components/Pages/TeachersPage/TeachersPage.js
+++ src/components/Pages/TeachersPage/TeachersPage.js
@@ -1,9 +1,146 @@
 import React from "react";
 import "./TeachersPage.css";
+import portret from "../../../assets/img/portret.png";
+import director from "../../../assets/img/director.jpg";
+import gherman1 from "../../../assets/img/gherman.png";
+import carapcevschi from "../../../assets/img/carapcevschi.jpg";
+import gumeniuc from "../../../assets/img/gumeniuc.png";
+import marsalcovschi from "../../../assets/img/marsalcovschi.png";
+import english from "../../../assets/img/english.png";
+import teachers1 from "../../../assets/img/teacher.jpg"
+import inostrannie from "../../../assets/img/inostrannie.jpg";
+import math from "../../../assets/img/math.jpg";
+import kafedra from "../../../assets/img/kafedra.png";
+import fizkulit from "../../../assets/img/fizkulit.png";
+import nachalinie from "../../../assets/img/nachalinie.png";
 
 class TeachersPage extends React.Component {
     render() {
-        return <div>TeachersPage</div>;
+        return <div>
+             <div class="about">
+            <img class="image25" src={portret} alt="" />
+            <span class="l">
+                ТЕОРЕТИЧЕСКИЙ ЛИЦЕЙ ИМЕНИ ДМИТРИЯ КАНТЕМИРА мун. Бэлць
+                приветствует Вас на своем информационном портале.</span
+            >
+            <span class="l1">
+                Здесь Вы найдете всю необходимую информацию о нашей
+                деятельности, планах на будущее, а также много полезного и
+                интересного. Приятного путешествия в мир знаний!</span
+            >
+            <span class="l2">
+                *Для удобной навигации по сайту, используйте меню.</span
+            >
+        </div>
+        <div class="lines"></div>
+        <div class="teachers">
+            <img class="image35" src={director} alt="" />
+            <span class="h">
+                Важную роль в любом образовательном учреждении играет
+                педагогический коллектив, включающий в себя учителей и
+                административных работников и обеспечивающий успешное
+                функционирование данного учреждения. Необходимо отметить, что
+                наш преподавательский состав — это единомышленники, которые свой
+                труд и профессионализм направляют на достижение одной общей
+                цели: строительство образовательного процесса и воспитательной
+                системы на принципах гуманизма и равноправия, взаимного уважения
+                и поддержки.</span
+            >
+            <span class="h1">
+                Благодаря единым взглядам, пониманию коллективной и
+                индивидуальной ответственности, уровню профессионализма и
+                компетентности, мы создали коллектив, обеспечивающий комфортный
+                психологический микроклимат для каждого педагога и
+                учащегося.</span
+            >
+            <span class="h2">
+                Внедрение современных методик образования, разработка
+                эффективных инструментов и техник помогает нам в решении
+                педагогических задач, повышении эффективности и качества
+                процесса обучения.</span
+            >
+            <span class="h3">
+                Приоритетными направлениями деятельности нашего коллектива,
+                помимо высокого уровня качества преподавания, являются раскрытие
+                потенциала, творческих возможностей и способностей личности и их
+                реализация, развитие в наших воспитанниках мотивации к познанию,
+                а также формирование уникального взгляда на окружающий их
+                мир.</span
+            >
+            <span class="h4">
+                Слаженная работа коллектива помогает формировать наших учеников
+                как интеллектуально развитых, образованных, культурных
+                личностей, гармонично существующих в рамках современного
+                общества.</span
+            >
+            <span class="h5">
+                Мы любим свою работу, гордимся успехами и достижениями наших
+                выпускников. Мы рады, что являемся одной большой и дружной
+                семьей.</span
+            >
+            <span class="h6"> Мурзак Анжела Григорьевна</span>
+            <span class="h7">
+                директор Теоретического Лицея имени ДМИТРИЯ КАНТЕМИРА</span
+            >
+            <div class="lines"></div>
+        </div>
+        <div class="collectiv">
+            <img class="image37" src={gherman1} alt="" />
+            <span class="k1"> Герман Оксана Григорьевна</span>
+            <span class="k2">
+                заместитель директора по учебно-воспитательной работе</span
+            >
+            <img class="image38" src={carapcevschi} alt="" />
+            <span class="k3"> Карапчевски Андриана Васильевна</span>
+            <span class="k4">
+                заместитель директора по учебно-воспитательной работе</span
+            >
+            <img class="image39" src={gumeniuc} alt="" />
+            <span class="k5"> Гуменюк Алла Александровна</span>
+            <span class="k6">
+                заместитель директора по учебно-воспитательной работе</span
+            >
+            <img class="image40" src={marsalcovschi} alt="" />
+            <span class="k7"> Маршалковски Галина Владимировна</span>
+            <span class="k8">
+                заместитель директора по учебно-воспитательной работе</span
+            >
+            <img class="image41" src={english} alt="" />
+            <span class="k9">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ СОЦИО-ГУМАНИТАРНОГО ЦИКЛА</span
+            >
+            <img class="image42" src={teachers1} alt="" />
+            <span class="k10">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ РУМЫНСКОГО ЯЗЫКА И
+                ЛИТЕРАТУРЫ</span
+            >
+            <img class="image43" src={inostrannie} alt="" />
+            <span class="k11">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ ИНОСТРАННЫХ ЯЗЫКОВ</span
+            >
+            <img class="image44" src={math} alt="" />
+            <span class="k12">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ МАТЕМАТИКИ И ИНФОРМАТИКИ</span
+            >
+            <img class="image45" src={kafedra} alt="" />
+            <span class="k13">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ РУССКОГО ЯЗЫКА И ЛИТЕРАТУРЫ</span
+            >
+            <img class="image46" src={fizkulit} alt="" />
+            <span class="k14">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ НАЧАЛЬНЫХ КЛАССОВ</span
+            >
+            <img class="image47" src={english}alt="" />
+            <span class="k15">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ ТЕХНОЛОГИИ, ИСКУССТВА И
+                СПОРТА</span
+            >
+            <img class="image48" src={nachalinie} alt="" />
+            <span class="k16">
+                МЕТОДИЧЕСКАЯ КОМИССИЯ УЧИТЕЛЕЙ ЕСТЕСТВЕННЫХ НАУК
+            </span>
+        </div>
+        </div>;
     }
 }
 
